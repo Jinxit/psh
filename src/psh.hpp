abb51039af723f7a2bd2af14b24b7d7e0f4574b7
Lucas Åström <lucasas@kth.se>
2015-08-11 11:38:05 +0200

    factored out 'prime' into an actual function, as well as 'create' to get rid of the goto

--- src/psh.hpp
+++ src/psh.hpp
@@ -31,133 +31,139 @@ namespace psh
 		uint r;
 		std::vector<point> phi;
 		std::vector<opt_point> H;
+		std::default_random_engine generator;
 
 		set(const std::vector<data_t>& data)
 			: n(data.size()), m_bar(std::ceil(std::pow(n, 1.0f / d))), m(std::pow(m_bar, d)),
-			  r_bar(std::ceil(std::pow(n / (2 * d), 1.0f / d)) - 1)
+			  r_bar(std::ceil(std::pow(n / (2 * d), 1.0f / d)) - 1), generator(time(0))
 		{
-			std::default_random_engine gen(time(0));
-
-			std::function<uint(void)> prime;
-			{
-				// http://planetmath.org/goodhashtableprimes
-				const std::vector<uint> primes = { 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289,
-					24593, 49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469 };
-				std::uniform_int_distribution<uint> prime_dist(0, primes.size() - 1);
-				prime = [prime_dist, primes, &gen]() mutable {
-					return primes[prime_dist(gen)];
-				};
-			}
-
 			M0 = prime();
 			M1 = prime();
 
 			VALUE(m);
 			VALUE(m_bar);
 
-		restart:
+			bool create_succeeded = false;
+
+			do
 			{
 				r_bar += d;
 				r = std::pow(r_bar, d);
 				VALUE(r);
 				VALUE(r_bar);
 
-				std::vector<point> phi_hat;
-				phi_hat.reserve(r);
-				std::vector<opt_point> H_hat(m);
-				std::cout << "creating " << r << " buckets" << std::endl;
+				create_succeeded = create(data);
 
+			} while (!create_succeeded);
+		}
+
+		uint prime()
+		{
+			static const std::vector<uint> primes = { 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289,
+				24593, 49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469 };
+			static std::uniform_int_distribution<uint> prime_dist(0, primes.size() - 1);
+
+			return primes[prime_dist(generator)];
+		}
+
+		bool create(const std::vector<data_t>& data)
+		{
+			std::vector<point> phi_hat;
+			phi_hat.reserve(r);
+			std::vector<opt_point> H_hat(m);
+			std::cout << "creating " << r << " buckets" << std::endl;
+
+			{
+				auto m_mod_r = m_bar % r_bar;
+				if (m_mod_r == 1 || m_mod_r == r - 1)
 				{
-					auto m_mod_r = m_bar % r_bar;
-					if (m_mod_r == 1 || m_mod_r == r - 1)
+					std::cout << "prime restart" << std::endl;
+					return false;
+				}
+			}
+
+			std::uniform_int_distribution<uint> dist(0, m - 1);
+
+			std::vector<std::vector<point>> buckets(r);
+
+			for (auto& element : data)
+			{
+				auto h1 = M1 * element;
+				buckets[point_to_index(h1, r_bar, r)].push_back(element);
+			}
+
+			std::cout << "buckets created, jiggling offsets" << std::endl;
+
+			bool easy = false;
+			for (uint i = 0; i < buckets.size(); i++)
+			{
+				if (buckets.size() / 10 > 0 && i % (buckets.size() / 10) == 0)
+					std::cout << (100 * i) / buckets.size() << "% done" << std::endl;
+				uint max_index = 0;
+
+				// in hard mode we find the biggest bucket
+				uint max_count = 0;
+				for (uint j = 0; j < buckets.size(); j++)
+				{
+					if (buckets[j].size() > max_count
+						|| (easy && buckets[j].size() > 0))
 					{
-						std::cout << "prime restart" << std::endl;
-						goto restart;
+						max_count = buckets[j].size();
+						max_index = j;
+
+						// in easy mode we grab the first filled bucket
+						if (easy)
+							break;
 					}
 				}
+				// if the biggest bucket was size 1, switch to easy mode next time
+				if (max_count == 1)
+					easy = true;
+				// if the biggest bucket was zero, we done
+				if (max_count == 0)
+					break;
 
-				std::uniform_int_distribution<uint> dist(0, m - 1);
-
-				std::vector<std::vector<point>> buckets(r);
-
-				for (auto& element : data)
+				uint possible_offset = dist(generator);
+				bool success = false;
+				for (uint k = 0; k < m; k++)
 				{
-					auto h1 = M1 * element;
-					buckets[point_to_index(h1, r_bar, r)].push_back(element);
-				}
+					possible_offset = (possible_offset + 1) % m;
 
-				std::cout << "buckets created, jiggling offsets" << std::endl;
+					phi_hat[max_index] = index_to_point(possible_offset, m_bar, m);
 
-				bool easy = false;
-				for (uint i = 0; i < buckets.size(); i++)
-				{
-					if (buckets.size() / 10 > 0 && i % (buckets.size() / 10) == 0)
-						std::cout << (100 * i) / buckets.size() << "% done" << std::endl;
-					uint max_index = 0;
-
-					// in hard mode we find the biggest bucket
-					uint max_count = 0;
-					for (uint j = 0; j < buckets.size(); j++)
+					bool already_used = false;
+					for (uint l = 0; l < buckets[max_index].size(); l++)
 					{
-						if (buckets[j].size() > max_count
-							|| (easy && buckets[j].size() > 0))
+						auto hashed = h(buckets[max_index][l], phi_hat);
+						if (H_hat[point_to_index(hashed, m_bar, m)])
 						{
-							max_count = buckets[j].size();
-							max_index = j;
-
-							// in easy mode we grab the first filled bucket
-							if (easy)
-								break;
-						}
-					}
-					// if the biggest bucket was size 1, switch to easy mode next time
-					if (max_count == 1)
-						easy = true;
-					// if the biggest bucket was zero, we done
-					if (max_count == 0)
-						break;
-
-					uint possible_offset = dist(gen);
-					bool success = false;
-					for (uint k = 0; k < m; k++)
-					{
-						possible_offset = (possible_offset + 1) % m;
-
-						phi_hat[max_index] = index_to_point(possible_offset, m_bar, m);
-
-						bool already_used = false;
-						for (uint l = 0; l < buckets[max_index].size(); l++)
-						{
-							auto hashed = h(buckets[max_index][l], phi_hat);
-							if (H_hat[point_to_index(hashed, m_bar, m)])
-							{
-								already_used = true;
-								break;
-							}
-						}
-
-						if (!already_used)
-						{
-							success = true;
-							for (uint l = 0; l < buckets[max_index].size(); l++)
-							{
-								auto hashed = h(buckets[max_index][l], phi_hat);
-								H_hat[point_to_index(hashed, m_bar, m)] = buckets[max_index][l];
-							}
+							already_used = true;
 							break;
 						}
 					}
-					if (!success)
-					{
-						goto restart;
-					}
-					buckets[max_index].clear();
-				}
 
-				std::cout << "done!" << std::endl;
-				phi = std::move(phi_hat);
-				H = std::move(H_hat);
+					if (!already_used)
+					{
+						success = true;
+						for (uint l = 0; l < buckets[max_index].size(); l++)
+						{
+							auto hashed = h(buckets[max_index][l], phi_hat);
+							H_hat[point_to_index(hashed, m_bar, m)] = buckets[max_index][l];
+						}
+						break;
+					}
+				}
+				if (!success)
+				{
+					return false;
+				}
+				buckets[max_index].clear();
 			}
+
+			std::cout << "done!" << std::endl;
+			phi = std::move(phi_hat);
+			H = std::move(H_hat);
+			return true;
 		}
 
 		constexpr uint point_to_index(const point& p, uint width, uint max) const
